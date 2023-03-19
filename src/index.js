e40e166d051bcbd694b752e88c03871c115c76ed
belyakovlxy <alex.weiss707@gmail.com>
2023-03-19 21:14:27 +0300

    added hole pedestal rotation

--- src/index.js
+++ src/index.js
@@ -4,14 +4,7 @@ console.log("Script is running")
 //  Pedestal
 //
 
-let currentLeftAngle = 0;
-let angleSpeed = 5;
-
-let rotOX = 0;
-let rotOY = 0;
-let rotOZ = 0;
-
-let posX = 0;
+let currentAngle = 0;
 
 let identitiyArray = new Float32Array(16);
 let identityMatrix = glMatrix.mat4.identity(identitiyArray);
@@ -21,12 +14,27 @@ let botCubeWMatx = new Float32Array(16);
 let leftCubeWMatx = new Float32Array(16);
 let rightCubeWMatx = new Float32Array(16);
 
+let topCubeWMatStart = new Float32Array(16);
+let botCubeWMatxStart = new Float32Array(16);
+let leftCubeWMatxStart = new Float32Array(16);
+let rightCubeWMatxStart = new Float32Array(16);
+
+let leftRad = 2;
+let rightRad = 2;
+
 let norm = 2;
 
 glMatrix.mat4.translate(topCubeWMatx, identityMatrix, [0, 2, 0]);
+glMatrix.mat4.translate(topCubeWMatStart, identityMatrix, [0, 2, 0]);
+
 glMatrix.mat4.translate(botCubeWMatx, identityMatrix, [0, 0, 0]);
+glMatrix.mat4.translate(botCubeWMatxStart, identityMatrix, [0, 0, 0]);
+
 glMatrix.mat4.translate(leftCubeWMatx, identityMatrix, [2, 0, 0]);
+glMatrix.mat4.translate(leftCubeWMatxStart, identityMatrix, [2, 0, 0]);
+
 glMatrix.mat4.translate(rightCubeWMatx, identityMatrix, [-2, 0, 0]);
+glMatrix.mat4.translate(rightCubeWMatxStart, identityMatrix, [-2, 0, 0]);
 
 let canvas = document.getElementById("pedestal");
 canvas.width = window.innerWidth;
@@ -83,37 +91,63 @@ gl.uniformMatrix4fv(matProjLocationCube, false, projMatrixCube);
 
 gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
+/*let curAngle = 45;
+
+glMatrix.mat4.rotate(topCubeWMatx, topCubeWMatx, angle(curAngle), [0, 1, 0]);
+glMatrix.mat4.copy(worldMatrixCube, topCubeWMatx);
+gl.uniformMatrix4fv(matWorldLocationCube, false, worldMatrixCube);
+gl.drawElements(gl.TRIANGLES, cubeIndices.length, gl.UNSIGNED_SHORT, 0);
+
+glMatrix.mat4.rotate(botCubeWMatx, botCubeWMatx, angle(curAngle), [0, 1, 0]);
+glMatrix.mat4.copy(worldMatrixCube, botCubeWMatx);
+gl.uniformMatrix4fv(matWorldLocationCube, false, worldMatrixCube);
+gl.drawElements(gl.TRIANGLES, cubeIndices.length, gl.UNSIGNED_SHORT, 0);
+
+glMatrix.mat4.translate(leftCubeWMatx,identityMatrix, [leftRad * Math.cos(angle(- curAngle)), 0, leftRad * Math.sin(angle(- curAngle))])
+glMatrix.mat4.rotate(leftCubeWMatx, leftCubeWMatx, angle(curAngle), [0, 1, 0]);
+glMatrix.mat4.copy(worldMatrixCube, leftCubeWMatx);
+gl.uniformMatrix4fv(matWorldLocationCube, false, worldMatrixCube);
+gl.drawElements(gl.TRIANGLES, cubeIndices.length, gl.UNSIGNED_SHORT, 0);
+
+glMatrix.mat4.translate(rightCubeWMatx,identityMatrix, [leftRad * Math.cos(angle(180 - curAngle)), 0, leftRad * Math.sin(angle(180 - curAngle))])
+glMatrix.mat4.rotate(rightCubeWMatx, rightCubeWMatx, angle(curAngle), [0, 1, 0]);
+glMatrix.mat4.copy(worldMatrixCube, rightCubeWMatx);
+gl.uniformMatrix4fv(matWorldLocationCube, false, worldMatrixCube);
+gl.drawElements(gl.TRIANGLES, cubeIndices.length, gl.UNSIGNED_SHORT, 0);*/
+
 document.addEventListener('keydown', (event) => {
     let name = event.key;
     let code = event.code;
 
     if (name == "w")
     {
-        currentLeftAngle -= 5;
-        glMatrix.mat4.rotate(topCubeWMatx, topCubeWMatx, angle(angleSpeed), [0, 1, 0]);
-        glMatrix.mat4.rotate(botCubeWMatx, botCubeWMatx, angle(angleSpeed), [0, 1, 0]);
+        currentAngle -= 5;
+        glMatrix.mat4.rotate(topCubeWMatx, topCubeWMatStart, angle(currentAngle), [0, 1, 0]);
+        glMatrix.mat4.rotate(botCubeWMatx, botCubeWMatxStart, angle(currentAngle), [0, 1, 0]);
 
-        //glMatrix.mat4.translate(leftCubeWMatx, identityMatrix, [norm * Math.cos(angle(currentLeftAngle)), 0,norm * Math.sin(angle(currentLeftAngle))]);
-        glMatrix.mat4.rotate(leftCubeWMatx, leftCubeWMatx, angle(angleSpeed), [0, 1, 0]);
+        glMatrix.mat4.translate(leftCubeWMatx, identityMatrix, [norm * Math.cos(angle(-currentAngle)), 0, norm * Math.sin(angle(-currentAngle))]);
+        glMatrix.mat4.rotate(leftCubeWMatx, leftCubeWMatx, angle(currentAngle), [0, 1, 0]);
 
-        //glMatrix.mat4.translate(rightCubeWMatx, identityMatrix, [norm *Math.cos(angle(180 + currentLeftAngle)), 0,norm * Math.sin(angle(180 + currentLeftAngle))]);
-        glMatrix.mat4.rotate(rightCubeWMatx, rightCubeWMatx, angle(angleSpeed), [0, 1, 0]);
+        glMatrix.mat4.translate(rightCubeWMatx, identityMatrix, [norm * Math.cos(angle(180 - currentAngle)), 0, norm * Math.sin(angle(180 - currentAngle))]);
+        glMatrix.mat4.rotate(rightCubeWMatx, rightCubeWMatx, angle(currentAngle), [0, 1, 0]);
 
     }
     if (name == "e")
     {
-        currentLeftAngle += 5;
-        glMatrix.mat4.rotate(topCubeWMatx, topCubeWMatx, angle(-angleSpeed), [0, 1, 0]);
-        glMatrix.mat4.rotate(botCubeWMatx, botCubeWMatx, angle(-angleSpeed), [0, 1, 0]);
+        currentAngle += 5;
+        glMatrix.mat4.rotate(topCubeWMatx, topCubeWMatStart, angle(currentAngle), [0, 1, 0]);
+        glMatrix.mat4.rotate(botCubeWMatx, botCubeWMatxStart, angle(currentAngle), [0, 1, 0]);
 
-        //glMatrix.mat4.translate(leftCubeWMatx, identityMatrix, [norm *Math.cos(angle(currentLeftAngle)), 0, norm *Math.sin(angle(currentLeftAngle))]);
-        glMatrix.mat4.rotate(leftCubeWMatx, leftCubeWMatx, angle(-angleSpeed), [0, 1, 0]);
+        glMatrix.mat4.translate(leftCubeWMatx, identityMatrix, [norm * Math.cos(angle(-currentAngle)), 0, norm * Math.sin(angle(-currentAngle))]);
+        glMatrix.mat4.rotate(leftCubeWMatx, leftCubeWMatx, angle(currentAngle), [0, 1, 0]);
 
-        //glMatrix.mat4.translate(rightCubeWMatx, identityMatrix, [norm *Math.cos(angle(180 + currentLeftAngle)), 0, norm *Math.sin(angle(180 + currentLeftAngle))]);
-        glMatrix.mat4.rotate(rightCubeWMatx, rightCubeWMatx, angle(-angleSpeed), [0, 1, 0]);
+        glMatrix.mat4.translate(rightCubeWMatx, identityMatrix, [norm * Math.cos(angle(180 - currentAngle)), 0, norm * Math.sin(angle(180 - currentAngle))]);
+        glMatrix.mat4.rotate(rightCubeWMatx, rightCubeWMatx, angle(currentAngle), [0, 1, 0]);
     }
     console.log(`Key pressed ${name} \r\n Key code value: ${code}`);
 }, false);
+
+
 
 let loop = () =>
 
